4ace5f2b78fc15af468a56e7a77ca38b5a9b258b
Omid Rouhani <o.rohani@gmail.com>
2022-03-16 16:47:26 +0330

    clearCache modified

--- src/infrastructure/cache/clear-cache.ts
+++ src/infrastructure/cache/clear-cache.ts
@@ -5,7 +5,6 @@ import { CacheOptionServiceEnum } from "./cache-options";
  * 
  * Clear Redis cache of the related area and service
  * 
- * @param {CacheOptionAreaEnum} area - Area of the backend service
  * @param {CacheOptionServiceEnum} serviceName - Service name
  */
 export const clearCache = ( serviceName: CacheOptionServiceEnum ) => {
